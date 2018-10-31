db18b34229db746648e912da326d415f729c97e9
und3rmysk1n <undermyskin@inbox.ru>
2018-10-31 20:39:48 +0300

    Добавил поддержку чисел с плавающей запятой и генерацию ошибки для числа с символами ID-шника.

--- 01-manual-lexer/ManualLexer/ManualLexerTests/GCalcLexerTests.cpp
+++ 01-manual-lexer/ManualLexer/ManualLexerTests/GCalcLexerTests.cpp
@@ -32,195 +32,223 @@ namespace
 
 TEST_CASE("Generated lexer can read one number", "[GCalcLexer]") {
 	REQUIRE(Tokenize("0") == TokenList{
-		GToken{ TK_NUMBER, "0" },
+		GToken{ TT_NUMBER, "0" },
 	});
 	REQUIRE(Tokenize("1") == TokenList{
-		GToken{ TK_NUMBER, "1" },
+		GToken{ TT_NUMBER, "1" },
 	});
 	REQUIRE(Tokenize("9876543210") == TokenList{
-		GToken{ TK_NUMBER, "9876543210" },
+		GToken{ TT_NUMBER, "9876543210" },
 	});
 }
 
 TEST_CASE("Generated lexer can read one operator", "[GCalcLexer]") {
 	REQUIRE(Tokenize("+") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 
 	REQUIRE(Tokenize("-") == TokenList{
-		GToken{ TK_MINUS },
+		GToken{ TT_MINUS },
 	});
 }
 
 TEST_CASE("Generated lexer can read expression tokens", "[GCalcLexer]") {
 	REQUIRE(Tokenize("45+9+28") == TokenList{
-		GToken{ TK_NUMBER, "45" },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "9" },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "28" },
+		GToken{ TT_NUMBER, "45" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "9" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "28" },
 	});
 
 	REQUIRE(Tokenize("00") == TokenList{
-		GToken{ TK_ERROR }
+		GToken{ TT_ERROR }
 	});
 
 	REQUIRE(Tokenize("13+1-28*27/10") == TokenList{
-		GToken{ TK_NUMBER, "13" },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "1" },
-		GToken{ TK_MINUS },
-		GToken{ TK_NUMBER, "28" },
-		GToken{ TK_MULTIPLICATION },
-		GToken{ TK_NUMBER, "27" },
-		GToken{ TK_DIVISION },
-		GToken{ TK_NUMBER, "10" },
+		GToken{ TT_NUMBER, "13" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "1" },
+		GToken{ TT_MINUS },
+		GToken{ TT_NUMBER, "28" },
+		GToken{ TT_MULTIPLICATION },
+		GToken{ TT_NUMBER, "27" },
+		GToken{ TT_DIVISION },
+		GToken{ TT_NUMBER, "10" },
 	});
 }
 
 TEST_CASE("Generated lexer can read one operator with whitespaces", "[CalcLexer]") {
 	REQUIRE(Tokenize("  +") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("\t+") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("   \t\t+") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("\n+") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("   \n  +") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("\t   \n  +") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("+    ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("+  \t\t   ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("+  \n\t   ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("   +   ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("  \t +  \t ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("  \t\t +  \t ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 	REQUIRE(Tokenize("  \t \t +  \n\t ") == TokenList{
-		GToken{ TK_PLUS },
+		GToken{ TT_PLUS },
 	});
 }
 
 TEST_CASE("Generated lexer can read one number with whitespaces", "[CalcLexer]") {
 	REQUIRE(Tokenize("  1") == TokenList{
-		GToken{ TK_NUMBER, "1" },
+		GToken{ TT_NUMBER, "1" },
 	});
 	REQUIRE(Tokenize("\t4") == TokenList{
-		GToken{ TK_NUMBER, "4" },
+		GToken{ TT_NUMBER, "4" },
 	});
 	REQUIRE(Tokenize("\n9") == TokenList{
-		GToken{ TK_NUMBER, "9" },
+		GToken{ TT_NUMBER, "9" },
 	});
 	REQUIRE(Tokenize("   \n  15") == TokenList{
-		GToken{ TK_NUMBER, "15" },
+		GToken{ TT_NUMBER, "15" },
 	});
 	REQUIRE(Tokenize("0    ") == TokenList{
-		GToken{ TK_NUMBER, "0" },
+		GToken{ TT_NUMBER, "0" },
 	});
 	REQUIRE(Tokenize("81  \t\t   ") == TokenList{
-		GToken{ TK_NUMBER, "81" },
+		GToken{ TT_NUMBER, "81" },
 	});
 	REQUIRE(Tokenize("  \t 3  \t ") == TokenList{
-		GToken{ TK_NUMBER, "3" },
+		GToken{ TT_NUMBER, "3" },
 	});
 	REQUIRE(Tokenize("  \t \t 7  \n\t ") == TokenList{
-		GToken{ TK_NUMBER, "7" },
+		GToken{ TT_NUMBER, "7" },
 	});
 }
 
 TEST_CASE("Generated lexer can read expression tokens with whitespaces") {
 	REQUIRE(Tokenize("2 + 3") == TokenList{
-		GToken{ TK_NUMBER, "2" },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "3" },
+		GToken{ TT_NUMBER, "2" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "3" },
 	});
 }
 
 TEST_CASE("Generated lexer cannot read number which starts with zero") {
 	REQUIRE(Tokenize("0123456789") == TokenList{
-		GToken{ TK_ERROR },
+		GToken{ TT_ERROR },
 	});
 	REQUIRE(Tokenize("+01") == TokenList{
-		GToken{ TK_PLUS },
-		GToken{ TK_ERROR },
+		GToken{ TT_PLUS },
+		GToken{ TT_ERROR },
 	});
 	REQUIRE(Tokenize("4+0521") == TokenList{
-		GToken{ TK_NUMBER, "4" },
-		GToken{ TK_PLUS },
-		GToken{ TK_ERROR },
+		GToken{ TT_NUMBER, "4" },
+		GToken{ TT_PLUS },
+		GToken{ TT_ERROR },
 	});
 	REQUIRE(Tokenize("02+21") == TokenList{
-		GToken{ TK_ERROR },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "21" },
+		GToken{ TT_ERROR },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "21" },
 	});
 }
 
 TEST_CASE("Generated lexer can read ID which starts with character") {
 	REQUIRE(Tokenize("thisIsSampleIdWithLettersOnly") == TokenList{
-		GToken{ TK_ID, "thisIsSampleIdWithLettersOnly" },
+		GToken{ TT_ID, "thisIsSampleIdWithLettersOnly" },
 	});
 	REQUIRE(Tokenize("Exampl3W1thD1g1t5") == TokenList{
-		GToken{ TK_ID, "Exampl3W1thD1g1t5" },
+		GToken{ TT_ID, "Exampl3W1thD1g1t5" },
 	});
 	REQUIRE(Tokenize("_exampleStartsWithUnderscore123") == TokenList{
-		GToken{ TK_ID, "_exampleStartsWithUnderscore123" }
+		GToken{ TT_ID, "_exampleStartsWithUnderscore123" }
 	});
 	REQUIRE(Tokenize("_12___132__strange___ID__") == TokenList{
-		GToken{ TK_ID, "_12___132__strange___ID__" }
+		GToken{ TT_ID, "_12___132__strange___ID__" }
 	});
 }
 
 TEST_CASE("Generated lexer can read expressions with IDs") {
 	REQUIRE(Tokenize("leftSide + rightSide") == TokenList{
-		GToken{ TK_ID, "leftSide" },
-		GToken{ TK_PLUS },
-		GToken{ TK_ID, "rightSide" },
+		GToken{ TT_ID, "leftSide" },
+		GToken{ TT_PLUS },
+		GToken{ TT_ID, "rightSide" },
 	});
 	REQUIRE(Tokenize("one - 2") == TokenList{
-		GToken{ TK_ID, "one" },
-		GToken{ TK_MINUS },
-		GToken{ TK_NUMBER, "2" },
+		GToken{ TT_ID, "one" },
+		GToken{ TT_MINUS },
+		GToken{ TT_NUMBER, "2" },
 	});
 }
 
 TEST_CASE("Generated lexer can read expressions with brackets") {
 	REQUIRE(Tokenize("(27 + 13) - 175") == TokenList{
-		GToken{ TK_LEFT_ROUND_BRACKET },
-		GToken{ TK_NUMBER, "27" },
-		GToken{ TK_PLUS },
-		GToken{ TK_NUMBER, "13" },
-		GToken{ TK_RIGHT_ROUND_BRACKET },
-		GToken{ TK_MINUS },
-		GToken{ TK_NUMBER, "175" },
+		GToken{ TT_LEFT_ROUND_BRACKET },
+		GToken{ TT_NUMBER, "27" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "13" },
+		GToken{ TT_RIGHT_ROUND_BRACKET },
+		GToken{ TT_MINUS },
+		GToken{ TT_NUMBER, "175" },
 	});
 
 	REQUIRE(Tokenize("[one, 2, three]") == TokenList{
-		GToken{ TK_LEFT_SQUARE_BRACKET },
-		GToken{ TK_ID, "one" },
-		GToken{ TK_COMMA },
-		GToken{ TK_NUMBER, "2" },
-		GToken{ TK_COMMA },
-		GToken{ TK_ID, "three" },
-		GToken{ TK_RIGHT_SQUARE_BRACKET },
+		GToken{ TT_LEFT_SQUARE_BRACKET },
+		GToken{ TT_ID, "one" },
+		GToken{ TT_COMMA },
+		GToken{ TT_NUMBER, "2" },
+		GToken{ TT_COMMA },
+		GToken{ TT_ID, "three" },
+		GToken{ TT_RIGHT_SQUARE_BRACKET },
+	});
+}
+
+TEST_CASE("Generated lexer can read fractional numbers") {
+	REQUIRE(Tokenize("5+7.005") == TokenList{
+		GToken{ TT_NUMBER, "5" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "7.005" },
+	});
+	REQUIRE(Tokenize("1.005+43.54+1") == TokenList{
+		GToken{ TT_NUMBER, "1.005" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "43.54" },
+		GToken{ TT_PLUS },
+		GToken{ TT_NUMBER, "1" },
+	});
+	REQUIRE(Tokenize(".5778") == TokenList{
+		GToken{ TT_ERROR }
+	});
+}
+
+TEST_CASE("Generated lexer can not read numbers with ID characters") {
+	REQUIRE(Tokenize("502someText") == TokenList{
+		GToken{ TT_ERROR }
+	});
+
+	REQUIRE(Tokenize("502someText911") == TokenList{
+		GToken{ TT_ERROR }
 	});
 }
